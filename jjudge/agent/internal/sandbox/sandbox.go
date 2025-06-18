c3b718781b71d552e30129c28d1f6c029a8ea844
joshjms <joshjms1607@gmail.com>
2025-06-18 18:05:29 +0800

    use overlayfs to allow writes inside container
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- jjudge/agent/internal/sandbox/sandbox.go
+++ jjudge/agent/internal/sandbox/sandbox.go
@@ -15,8 +15,8 @@ import (
 )
 
 const (
-	SandboxRoot = "/var/lib/sandbox"
-	CgroupRoot  = "/sys/fs/cgroup/jjudge.slice/"
+	sandboxRoot = "/var/lib/sandbox"
+	cgroupSlice = "jjudge.slice"
 )
 
 // Sandbox represents a sandbox environment for running isolated processes.
@@ -25,29 +25,22 @@ type Sandbox struct {
 
 	spec SandboxSpec
 
-	// BundlePath is the absolute path to the directory containing the OCI bundle.
+	// rootfs is the path to the root filesystem to use for the sandbox.
+	// It is expected to be a valid OCI root filesystem that can be used to create the container.
+	rootfs string
+
+	// bundlePath is the absolute path to the directory containing the OCI bundle.
 	// It includes the root filesystem, configuration file, and other necessary files.
 	// The bundle directory structure is expected to follow the OCI runtime specification.
 	bundlePath string
-
-	// Stdout is the absolute path to the file where standard output will be written.
-	stdout string
-
-	// Stderr is the absolute path to the file where standard error will be written.
-	stderr string
 }
 
-func NewSandbox(id string, spec SandboxSpec, bundlePath string) (*Sandbox, error) {
-	if spec.Rootfs == "" || spec.BoxDir == "" || spec.WorkingDir == "" || len(spec.Args) == 0 {
-		return nil, fmt.Errorf("invalid sandbox spec: missing required fields")
-	}
-
+func NewSandbox(id string, spec SandboxSpec, rootfs string) (*Sandbox, error) {
 	return &Sandbox{
 		id:         id,
 		spec:       spec,
-		bundlePath: bundlePath,
-		stdout:     filepath.Join(bundlePath, "stdout.txt"),
-		stderr:     filepath.Join(bundlePath, "stderr.txt"),
+		rootfs:     rootfs,
+		bundlePath: filepath.Join(sandboxRoot, "jobs", id),
 	}, nil
 }
 
@@ -56,15 +49,7 @@ func NewSandbox(id string, spec SandboxSpec, bundlePath string) (*Sandbox, error
 // and user IDs (UID and GID) for the sandbox.
 // It is used to generate an OCI runtime specification for the sandbox.
 type SandboxSpec struct {
-	// Rootfs is the path to the container's root filesystem.
-	Rootfs string
-
-	// BoxDir is a host directory that is bind-mounted into the container.
-	// We use this to place the user's code and other files that the container needs to access
-	// to run the code.
-	BoxDir string
-
-	// WorkingDir is the working directory inside the container (relative to Rootfs).
+	// WorkingDir is the working directory inside the container.
 	WorkingDir string
 
 	// Args are the command and arguments to execute inside the container.
@@ -103,12 +88,76 @@ type Result struct {
 	CpuUsageUsec    uint64
 }
 
-func (s *Sandbox) GenerateOCISpec() (specs.Spec, error) {
+func (s *Sandbox) GetID() string {
+	return s.id
+}
+
+func (s *Sandbox) Run() (*Result, error) {
+	s.bundlePath = filepath.Join(sandboxRoot, "jobs", s.id)
+	if err := os.MkdirAll(s.bundlePath, 0755); err != nil {
+		return nil, fmt.Errorf("error creating sandbox directory: %w", err)
+	}
+
+	spec, err := s.generateOCISpec()
+	if err != nil {
+		return nil, fmt.Errorf("error generating oci spec: %w", err)
+	}
+
+	if err := os.MkdirAll(s.bundlePath, 0755); err != nil {
+		return nil, fmt.Errorf("error creating sandbox directory: %w", err)
+	}
+
+	specFile := filepath.Join(s.bundlePath, "config.json")
+	if err := writeSpecToFile(&spec, specFile); err != nil {
+		return nil, fmt.Errorf("error writing oci spec to file: %w", err)
+	}
+
+	if err := s.prepareRootfs(); err != nil {
+		return nil, fmt.Errorf("error preparing rootfs: %w", err)
+	}
+
+	var stdout, stderr bytes.Buffer
+	cmd := exec.Command("runc", "run", "--bundle", s.bundlePath, "--keep", s.id)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err = cmd.Run()
+	defer func() {
+		exec.Command("runc", "delete", s.id).Run()
+	}()
+
+	res := &Result{
+		Stdout: stdout.String(),
+		Stderr: stderr.String(),
+	}
+
+	if err != nil {
+		return res, fmt.Errorf("error runc run: %w", err)
+	}
+
+	m, err := cgroup2.LoadSystemd(cgroupSlice, fmt.Sprintf("%s.scope", s.id))
+	if err != nil {
+		return nil, fmt.Errorf("cannot load cgroup: %w", err)
+	}
+
+	metrics, err := m.Stat()
+	if err != nil {
+		return nil, fmt.Errorf("cannot get cgroup metrics: %w", err)
+	}
+
+	memPeak := metrics.GetMemory().GetMaxUsage()
+	cpuUsage := metrics.GetCPU().GetUsageUsec()
+
+	res.MemoryPeakBytes = memPeak
+	res.CpuUsageUsec = cpuUsage
+
+	return res, nil
+}
+
+func (s *Sandbox) generateOCISpec() (specs.Spec, error) {
 	memBytes := s.spec.MemoryLimitMB * 1024 * 1024
 	cpuQuotaMicros := s.spec.CPUQuotaMillis * 1000
 	cpuPeriod := uint64(100000)
-
-	overlayDir := filepath.Join(s.bundlePath, "overlay")
 
 	ociSpec := specs.Spec{
 		Version: specs.Version,
@@ -128,8 +177,8 @@ func (s *Sandbox) GenerateOCISpec() (specs.Spec, error) {
 			},
 		},
 		Root: &specs.Root{
-			Path:     s.spec.Rootfs,
-			Readonly: true,
+			Path:     filepath.Join(s.bundlePath, "rootfs"),
+			Readonly: false,
 		},
 		Mounts: []specs.Mount{
 			{
@@ -168,15 +217,9 @@ func (s *Sandbox) GenerateOCISpec() (specs.Spec, error) {
 				Source:      "tmpfs",
 				Options:     []string{"nosuid", "strictatime", "mode=1777", "size=65536k"},
 			},
-			{
-				Destination: "/box",
-				Type:        "bind",
-				Source:      overlayDir,
-				Options:     []string{"rbind", "rw", "exec", "nosuid", "nodev"},
-			},
 		},
 		Linux: &specs.Linux{
-			CgroupsPath: fmt.Sprintf("/jjudge.slice/%s.scope", s.id),
+			CgroupsPath: fmt.Sprintf("/%s/%s.scope", cgroupSlice, s.id),
 			Resources: &specs.LinuxResources{
 				Memory: &specs.LinuxMemory{Limit: &memBytes, Swap: &memBytes},
 				CPU: &specs.LinuxCPU{
@@ -229,75 +272,31 @@ func (s *Sandbox) GenerateOCISpec() (specs.Spec, error) {
 	return ociSpec, nil
 }
 
-func (s *Sandbox) Run() (*Result, error) {
-	spec, err := s.GenerateOCISpec()
-	if err != nil {
-		return nil, fmt.Errorf("error generating oci spec: %w", err)
-	}
-
-	if err := os.MkdirAll(s.bundlePath, 0755); err != nil {
-		return nil, fmt.Errorf("error creating sandbox directory: %w", err)
-	}
-
-	specFile := filepath.Join(s.bundlePath, "config.json")
-	if err := writeSpecToFile(&spec, specFile); err != nil {
-		return nil, fmt.Errorf("error writing oci spec to file: %w", err)
-	}
-
-	if err := prepareRootfs(s.bundlePath); err != nil {
-		return nil, fmt.Errorf("error preparing rootfs: %w", err)
-	}
-
-	var stdout, stderr bytes.Buffer
-	cmd := exec.Command("runc", "run", "--bundle", s.bundlePath, "--keep", s.id)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err = cmd.Run()
-	// defer func() {
-	// 	exec.Command("runc", "delete", s.id).Run()
-	// }()
-	if err != nil {
-		return nil, fmt.Errorf("error runc run: %w", err)
-	}
-
-	m, err := cgroup2.LoadSystemd("jjudge.slice", fmt.Sprintf("%s.scope", s.id))
-	if err != nil {
-		return nil, fmt.Errorf("cannot load cgroup: %w", err)
-	}
-
-	metrics, err := m.Stat()
-	if err != nil {
-		return nil, fmt.Errorf("cannot get cgroup metrics: %w", err)
-	}
-
-	memPeak := metrics.GetMemory().GetMaxUsage()
-	cpuUsage := metrics.GetCPU().GetUsageUsec()
-
-	res := &Result{
-		Stdout:          stdout.String(),
-		Stderr:          stderr.String(),
-		MemoryPeakBytes: memPeak,
-		CpuUsageUsec:    cpuUsage,
-	}
-
-	return res, nil
-}
-
-func prepareRootfs(bundlePath string) error {
-	dirs := []string{"upper", "work", "overlay"}
+func (s *Sandbox) prepareRootfs() error {
+	dirs := []string{"upper", "work", "rootfs"}
 	for _, d := range dirs {
-		path := filepath.Join(bundlePath, d)
+		path := filepath.Join(s.bundlePath, d)
 		if err := os.MkdirAll(path, 0755); err != nil {
 			return err
 		}
 	}
 
-	lowerImage := filepath.Join(SandboxRoot, "lower", "rootfs", "box")
+	lowerImage := s.rootfs
 
-	upperDir := filepath.Join(bundlePath, "upper")
-	workDir := filepath.Join(bundlePath, "work")
-	overlayDir := filepath.Join(bundlePath, "overlay")
+	upperDir := filepath.Join(s.bundlePath, "upper")
+	workDir := filepath.Join(s.bundlePath, "work")
+	overlayDir := filepath.Join(s.bundlePath, "rootfs")
+
+	if err := os.Chown(upperDir, int(s.spec.HostUID), int(s.spec.HostGID)); err != nil {
+		return fmt.Errorf("chown upperDir: %w", err)
+	}
+	if err := os.Chown(workDir, int(s.spec.HostUID), int(s.spec.HostGID)); err != nil {
+		return fmt.Errorf("chown workDir: %w", err)
+	}
+
+	if err := os.MkdirAll(filepath.Join(upperDir, s.spec.WorkingDir), 0755); err != nil {
+		return fmt.Errorf("error creating upper directory: %w", err)
+	}
 
 	options := fmt.Sprintf("lowerdir=%s,upperdir=%s,workdir=%s", lowerImage, upperDir, workDir)
 	if err := syscall.Mount("overlay", overlayDir, "overlay", 0, options); err != nil {
